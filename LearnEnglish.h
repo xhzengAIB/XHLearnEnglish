8ab82f36d94b1f72000d165b63987a5f0a0f90db
Jack <xhzengAIB@gmail.com>
2014-01-14 00:30:37 +0000

    add file

---
+++ LearnEnglish.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  IQProjectVideo
+//
+//  Created by Iftekhar Mac Pro on 9/29/13.
+//  Copyright (c) 2013 Iftekhar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
