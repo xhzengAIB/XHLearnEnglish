7655f18627241a430ac202187d1658eaee14d9ac
Jack <xhzengAIB@gmail.com>
2014-01-14 00:35:13 +0000

    UpData

--- LearnEnglish.h
+++ LearnEnglish.h
@@ -1,15 +1,1 @@
-//
-//  AppDelegate.h
-//  IQProjectVideo
-//
-//  Created by Iftekhar Mac Pro on 9/29/13.
-//  Copyright (c) 2013 Iftekhar. All rights reserved.
-//
-
-#import <UIKit/UIKit.h>
-
-@interface AppDelegate : UIResponder <UIApplicationDelegate>
-
-@property (strong, nonatomic) UIWindow *window;
-
-@end
+经典爱情26个字母
